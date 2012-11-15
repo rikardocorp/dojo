56ba0853dea2bcfe9c2e26744ca813b8b74f239c
Leonidas Esteban <leonidasesteban@gmail.com>
2012-11-15 00:47:15 -0500

    estilos dojo

--- index.php
+++ index.php
@@ -3,15 +3,18 @@
 			<h3 class="titu">
 				<span class="content">Encuentranos</span>
 			</h3>
-			<div class="content">
-				<img id="map" src="http://maps.googleapis.com/maps/api/staticmap?size=465x319&amp;maptype=roadmap&amp;markers=color:red%7Ccolor:red%7C-18.002966,-70.240314&amp;zoom=17&amp;sensor=false">
-			</div>
 			<div class="content datos">
-				<p>Numeros Telefonicos: 980731152</p>
-				<p>Direccion: Gimnasio top golden gym calle piura 376</p>
-				<p>Email: shinkyokushinkaiperu@gmail.com</p>
-				<p>Facebook: shinkyokushinkai peru</p>
-				<p>Twitter: Pronto</p>
-			</div>						
+				<div class="mapa">
+					<img id="map" src="http://maps.googleapis.com/maps/api/staticmap?size=465x319&amp;maptype=roadmap&amp;markers=color:red%7Ccolor:red%7C-18.002966,-70.240314&amp;zoom=17&amp;sensor=false">	
+				</div>
+				<div class="contacto" id="contacto">
+					<p><strong>Numeros Telefonicos:</strong> 980731152</p>
+					<p><strong>Direccion:</strong> Gimnasio top golden gym calle piura 376</p>
+					<p><strong>Email:</strong> <a href="mailto:shinkyokushinkaiperu@gmail.com">shinkyokushinkaiperu@gmail.com</a></p>
+					<p><strong>Facebook:</strong> shinkyokushinkai peru</p>
+					<p><strong>Twitter:</strong> Pronto</p>
+				</div>
+			</div>
+								
 		</section>
 <?php get_footer(); ?>
